61c5fe01fc286ae31dde610b99d2511cd5c4cfb9
swigguma5776 <ashley.swiggum11@gmail.com>
2023-04-19 16:25:50 -0500

    added day 3 morning

--- drone_inventory/__init__.py
+++ drone_inventory/__init__.py
@@ -2,9 +2,13 @@ from flask import Flask
 from config import Config
 from .site.routes import site
 from .authentication.routes import auth
-from .models import db as root_db
+from .models import db as root_db, login_manager, ma 
 
 from flask_migrate import Migrate
+
+#flask CORS import - CROSS ORIGIN RESOURCE SHARING - future proofing
+#our react app so it can make api calls to this flask app
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -15,6 +19,14 @@ app.config.from_object(Config)
 root_db.init_app(app)
 
 migrate = Migrate(app, root_db)
+
+ma.init_app(app)
+
+login_manager.init_app(app)
+login_manager.login_view = 'auth.signin'
+
+
+CORS(app)
 
 
 
