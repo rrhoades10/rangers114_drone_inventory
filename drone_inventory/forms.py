64e118cd6a70785369ca2103ff775bdaee58c149
rrhoades10 <rrhoades10@yahoo.com>
2023-04-21 14:13:52 -0500

    added a bunch of stuff

--- drone_inventory/forms.py
+++ drone_inventory/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, DecimalField
 from wtforms.validators import DataRequired, Email
 
 class UserLoginForm(FlaskForm):
@@ -7,6 +7,21 @@ class UserLoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators = [DataRequired(), Email()])
     password = PasswordField('Password', validators = [DataRequired()])
+    submit_button = SubmitField()
+
+
+class DroneForm(FlaskForm):
+    name = StringField('name')
+    description = StringField('description')
+    price = DecimalField('price', places = 2)
+    camera_quality = StringField('camera quality')
+    flight_time = StringField('flight time')
+    max_speed = StringField('max speed')
+    dimensions = StringField('dimensions')
+    weight = StringField('weight')
+    cost_of_production = DecimalField('cost of production', places = 2)
+    series = StringField('series')
+    dad_joke = StringField('dad joke')
     submit_button = SubmitField()
 
 
